249afe3afd687a96c3d7c9c6b5b85e6d903db341
Karsten Luecke <karsten.luecke@scout24.com>
2017-01-04 16:17:36 +0100

    PyBuilder setup

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
+use_plugin("python.distutils")
 
 
 name = "rds_log_dog"
