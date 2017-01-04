7c5218e302b6f460bbc5e0e4e136e39dc0933d1f
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-04 09:09:27 +0100

    scaffolding python pybuilder project
    created with: pyb --start-project

--- build.py
+++ build.py
@@ -1,5 +1,16 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
 
+
+name = "rds_log_dog"
 default_task = "publish"
+
+
+@init
+def set_properties(project):
+    pass
