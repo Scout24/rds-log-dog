04df13781e973b1b2f019df8f0e39fc76b65bc85
Karsten Luecke <karsten.luecke@scout24.com>
2017-01-04 16:16:28 +0100

    PyBuilder setup

---
+++ src/unittest/python/rds_log_dog_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from rds_log_dog import hello
+
+
+class TestHello(unittest.TestCase):
+
+    def test_should_return_hello(self):
+        self.assertEqual('hello world', hello())
