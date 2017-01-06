466f060538dca774804a4350988b31874000e286
Karsten Luecke <karsten.luecke@scout24.com>
2017-01-06 09:19:43 +0100

    PyBuilder aws plugin + lambda funtion added.

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-
+use_plugin('pypi:pybuilder_aws_plugin')
 
 name = "rds_log_dog"
 default_task = "publish"
