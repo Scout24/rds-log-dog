40111f0346a773dea8526b4e6876d3038c1a275e
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-13 15:34:12 +0100

    added a logger
    let's go with the normal logger and a config
    SDI-2144

---
+++ src/main/python/rds_log_dog/config.py
@@ -0,0 +1,15 @@
+import logging
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+#class Config(object):
+
+
--- src/main/python/rds_log_dog/rds_log_dog.py
+++ src/main/python/rds_log_dog/rds_log_dog.py
@@ -1,9 +1,14 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
+from .config import get_logger
+
 class RDSLogDog(object):
 
     def do(self):
+        logger = get_logger(__name__)
+        logger.info('start')
         # setup things
+
           # s3 dest path
         # discover()
         # foreach instance do
