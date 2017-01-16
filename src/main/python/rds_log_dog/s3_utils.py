41c5070ea1dca838f6917c18bbd9cb75fefbb5a2
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-16 18:26:24 +0100

    bugfix with special foldernames
    SDI-2153

--- src/main/python/rds_log_dog/s3_utils.py
+++ src/main/python/rds_log_dog/s3_utils.py
@@ -1,8 +1,18 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
 import boto3 
+from .config import get_logger
+
+def get_top_level_folder_under_prefix(prefix, parent_prefix):
+    result = None
+    chars_to_strip = len(parent_prefix) + 1 
+    stripped_prefix = prefix[chars_to_strip:]
+    if stripped_prefix.find('/') > 0:
+        result = stripped_prefix.split('/')[0]
+    return result
 
 def list_folders(Bucket, Prefix):
+    logger = get_logger(__name__)
     client = boto3.client('s3')
     response = client.list_objects_v2(Bucket=Bucket, Prefix=Prefix)
     if response['IsTruncated']:
@@ -10,8 +20,9 @@ def list_folders(Bucket, Prefix):
     folders = set()
     if 'Contents' in response:
         for key_struct in response['Contents']:
-            key_without_pref = key_struct['Key'].lstrip(Prefix + '/')
-            if key_without_pref.find('/') > 0:
-                key_first_level = key_without_pref.split('/')[0]
-                folders.add(key_first_level)
+            folder = get_top_level_folder_under_prefix(key_struct['Key'], Prefix)
+            if folder:
+                folders.add(folder)
+    else:
+        logger.warn('list on prefix: {}/{} not possible. Does it exists ?'.format(Bucket, Prefix))
     return(folders)
--- src/unittest/python/s3_utils_tests.py
+++ src/unittest/python/s3_utils_tests.py
@@ -3,7 +3,7 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 import unittest2 as unittest
 import boto3 
 from moto import mock_s3
-from rds_log_dog.s3_utils import list_folders
+from rds_log_dog.s3_utils import list_folders, get_top_level_folder_under_prefix
 
 class TestS3Utils(unittest.TestCase):
 
@@ -45,6 +45,43 @@ class TestS3Utils(unittest.TestCase):
         self.s3.put_object(Bucket='mybucket', Key='rds_logs/folder2/file1')
         self.assertEqual({'folder1', 'folder2'}, list_folders(Bucket='mybucket', Prefix='rds_logs'))
 
+    def test_get_top_level_folder_under_prefix_top_level_folder(self):
+        self.assertEqual('folder1', get_top_level_folder_under_prefix(
+            'rds_log/folder1/', 'rds_log'))
+
+    def test_get_top_level_folder_under_prefix_2nd_level_folder(self):
+        self.assertEqual('folder1', get_top_level_folder_under_prefix(
+            'rds_log/folder1/subfolder/', 'rds_log'))
+
+    def test_get_top_level_folder_under_prefix_2nd_level_file(self):
+        self.assertEqual('folder1', get_top_level_folder_under_prefix(
+            'rds_log/folder1/file', 'rds_log'))
+
+    def test_get_top_level_folder_under_prefix_top_level_file(self):
+        self.assertEqual(None, get_top_level_folder_under_prefix(
+            'rds_log/folder1', 'rds_log'))
+
+    def test_get_top_level_folder_under_prefix_with_tricky_foldername(self):
+        self.assertEqual('id', get_top_level_folder_under_prefix(
+            'rds_log/id/', 'rds_log'))
+        self.assertEqual('rds_id', get_top_level_folder_under_prefix(
+            'rds_log/rds_id/', 'rds_log'))
+
+    # TODO/FIXME: unimplemented or wrong usage
+    def test_get_top_level_folder_under_prefix_with_parent_end_w_slash(self):
+        # wrong
+        self.assertEqual('older1', get_top_level_folder_under_prefix(
+            'rds/folder1/', 'rds/'))
+        # right
+        self.assertEqual(None, get_top_level_folder_under_prefix(
+            'rds/folder1', 'rds/'))
+
+    # TODO/FIXME: unimplemented or wrong usage
+    def test_get_top_level_folder_under_prefix_with_no_parent(self):
+        with self.assertRaises(Exception):
+            self.assertEqual(None, get_top_level_folder_under_prefix(
+            'rds/folder1/', None))
+    
 if __name__ == '__main__':
     unittest.main()  
 
