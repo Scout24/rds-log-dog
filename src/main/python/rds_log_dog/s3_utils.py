1f6297345c74fb5227fafde28b0d6529e3dedf1a
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-16 14:45:55 +0100

    fix for unittest
    SDI-2153

--- src/main/python/rds_log_dog/s3_utils.py
+++ src/main/python/rds_log_dog/s3_utils.py
@@ -8,9 +8,10 @@ def list_folders(Bucket, Prefix):
     if response['IsTruncated']:
         raise Exception("bucket have more than 1000 keys - not implemented until now")
     folders = set()
-    for key_struct in response['Contents']:
-        key_without_pref = key_struct['Key'].lstrip(Prefix + '/')
-        if key_without_pref.find('/') > 0:
-            key_first_level = key_without_pref.split('/')[0]
-            folders.add(key_first_level)
+    if 'Contents' in response:
+        for key_struct in response['Contents']:
+            key_without_pref = key_struct['Key'].lstrip(Prefix + '/')
+            if key_without_pref.find('/') > 0:
+                key_first_level = key_without_pref.split('/')[0]
+                folders.add(key_first_level)
     return(folders)
