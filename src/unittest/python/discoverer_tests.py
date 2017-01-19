a48fbabb9ee700202b57cfc36400edecd49e2a84
Michael Waldeck <michael.waldeck@scout24.com>
2017-01-19 17:34:44 +0100

    SDI-2143 add test list result

--- src/unittest/python/discoverer_tests.py
+++ src/unittest/python/discoverer_tests.py
@@ -9,10 +9,10 @@ from rds_log_dog.discoverer import Discoverer
 class Test(unittest.TestCase):
 
     @mock_rds
-    def test_discoverer_discover(self):
+    def test_discoverer_discover_with_no_rds_instances(self):
         disco = Discoverer()
         disco_result = disco.discover()
-        self.assertEqual(len(disco_result), 0, "count rds-instances should be 0 in moto-mock env")
+        self.assertEqual([], disco_result, "count rds-instances should be 0 in moto-mock env and from type list")
 
 if __name__ == '__main__':
     unittest.main()
