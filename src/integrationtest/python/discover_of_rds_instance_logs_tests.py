091a64741f1a0bdb8524a86e1d1737214e550dfb
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-19 19:39:49 +0100

    more testing on discover rds logs

--- src/integrationtest/python/discover_of_rds_instance_logs_tests.py
+++ src/integrationtest/python/discover_of_rds_instance_logs_tests.py
@@ -1,8 +1,11 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
 import unittest2 as unittest
+import boto3
+
 from rds_log_dog.discoverer import Discoverer
-from rds_log_dog.log_file_handler import rds_logfiles
+from rds_log_dog.log_file_handler import LogFileHandler
+from rds_log_dog.rds_instance import RDSInstance
 
 
 class Test(unittest.TestCase):
@@ -10,17 +13,18 @@ class Test(unittest.TestCase):
     def test_log_file_handler_discover_rds_logfiles(self):
         disco = Discoverer()
         instances = disco.discover()
-        self.assertGreater(0, len(instances),
-                           "We need at least one RDS instance for this test!")
+        self.assertLess(0, len(instances),
+                        "We need at least one RDS instance for this test!")
 
         # pick one instance
         instance = instances.pop()
 
         client = boto3.client('rds')
-        response = client.describe_db_logfiles(instance.name)
-        discovered_logfile = discover_rds_logfiles(instance.name)
-        assertEqual('set', type(discovered_logfile))
-        assertEqual(len(response['DescribeDBLogFiles']),
+        response = client.describe_db_log_files(DBInstanceIdentifier=instance.name)
+        logfilehandler = LogFileHandler(RDSInstance(instance.name), 'b', 'p')
+        discovered_logfiles = logfilehandler.discover_rds_logfiles()
+        self.assertEqual(type(set()), type(discovered_logfiles))
+        self.assertEqual(len(response['DescribeDBLogFiles']),
                     len(discovered_logfiles))
 
 if __name__ == '__main__':
