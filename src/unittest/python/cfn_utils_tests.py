03f23b79ccecb9031f204106c1d31ef319700fbf
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-01-19 16:12:41 +0100

    tweak coverage for pyb too. add unittest

---
+++ src/unittest/python/cfn_utils_tests.py
@@ -0,0 +1,45 @@
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+import unittest2 as unittest
+import boto3
+import moto
+from pprint import pprint
+from rds_log_dog.cfn_utils import cfn_get_output
+
+
+class Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.s3 = boto3.client('s3')
+
+    @moto.mock_cloudformation
+    def test_cfn_get_output(self):
+        client = boto3.client('cloudformation')
+        response = client.create_stack(
+                StackName='foo',
+                TemplateBody="""
+{
+  "AWSTemplateFormatVersion": "2010-09-09",
+  "Description": "An s3 bucket",
+  "Resources": {
+    "bucket": {
+      "Type": "AWS::S3::Bucket",
+      "Properties" : {
+        "BucketName": "MyBucketName"
+    }}
+  },
+  "Outputs": {
+    "name": {
+      "Description": "name of destination log bucket",
+      "Value": {"Ref": "bucket"}
+    }
+  }
+} """)
+	self.assertTrue('StackId' in response)
+        self.assertEqual('MyBucketName', cfn_get_output(response['StackId'], 'name'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
