21afad0a8828f7526ddffbcad1c5a6d936a560ca
Michael Waldeck <michael.waldeck@scout24.com>
2017-02-09 17:35:06 +0100

    SDI-2175 next try

--- src/unittest/python/s3_utils_tests.py
+++ src/unittest/python/s3_utils_tests.py
@@ -9,6 +9,10 @@ from tempfile import NamedTemporaryFile
 from rds_log_dog.s3_utils import (
     list_folders, get_top_level_folder_under_prefix, write_data_to_object, get_size, get_files)
 
+# Else we run into problems with mocking
+os.environ['http_proxy'] = ''
+os.environ['https_proxy'] = ''
+os.environ['no_proxy'] = ''
 
 class TestS3Utils(unittest.TestCase):
 
