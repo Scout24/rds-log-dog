debb9eed5210339d31a5beabe1292f9464d0bd50
Michael Waldeck <michael.waldeck@scout24.com>
2017-02-10 11:13:52 +0100

    SDI-2175

--- src/unittest/python/cfn_utils_tests.py
+++ src/unittest/python/cfn_utils_tests.py
@@ -9,9 +9,9 @@ from rds_log_dog.cfn_utils import cfn_get_output
 import os
 
 # Else we run into problems with mocking
-#os.environ['http_proxy'] = ''
-#os.environ['https_proxy'] = ''
-#os.environ['no_proxy'] = ''
+os.environ['http_proxy'] = ''
+os.environ['https_proxy'] = ''
+os.environ['no_proxy'] = ''
 os.environ['AWS_DEFAULT_REGION'] = 'eu-west-1'
 
 
