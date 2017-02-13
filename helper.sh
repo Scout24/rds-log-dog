f3a9735b88f35195bca000403c4f0865c041f906
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-02-13 17:37:06 +0100

    simplify and bugfix unset/restore http_proxy env

--- helper.sh
+++ helper.sh
@@ -16,17 +16,16 @@ function set_dst_s3_bucket_name_from_stack {
 }
 
 function unset_proxy_env {
-    for v in $(env |awk -v FS='=' ' {print $1}' | grep -ie '^http.*_proxy$'); do 
-        export __$v=$v
-        echo "unsetting env: $v"
-        unset $v
-    done
+        unset __HTTP_PROXY
+        unset __HTTPS_PROXY
+        [ -n "$http_proxy" ]   && export __HTTP_PROXY=$http_proxy && unset http_proxy
+        [ -n "$HTTP_PROXY" ]   && export __HTTP_PROXY=$HTTP_PROXY && unset HTTP_PROXY
+        [ -n "$https_proxy" ]  && export __HTTPS_PROXY=$https_proxy && unset https_proxy
+        [ -n "$HTTPS_PROXY" ]  && export __HTTPS_PROXY=$HTTPS_PROXY && unset HTTPS_PROXY
 }
 
 function restore_proxy_env {
-    for v in $(env |awk -v FS='=' ' {print $1}' | grep -ie '^__http.*_proxy$'); do 
-        export eval "${v:2}=$v"
-        unset $v
-    done
+    [ -n "$__HTTP_PROXY" ] && export HTTP_PROXY=$__HTTP_PROXY && unset __HTTP_PROXY
+    [ -n "$__HTTPS_PROXY" ] && export HTTPS_PROXY=$__HTTPS_PROXY && unset __HTTPS_PROXY
 }
 
