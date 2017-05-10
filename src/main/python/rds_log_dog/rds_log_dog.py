ac9905568a84c0141f677035c0d29e33b952ddfa
Felix Borchers <felix.borchers@immobilienscout24.de>
2017-05-10 14:20:55 +0200

    fix logging issue with multiple handler attached

--- src/main/python/rds_log_dog/rds_log_dog.py
+++ src/main/python/rds_log_dog/rds_log_dog.py
@@ -6,8 +6,6 @@ from rds_log_dog.discoverer import Discoverer
 from rds_log_dog.log_file_handler import LogFileHandler
 from rds_log_dog.s3_utils import setup_s3_destination
 
-logger = logging.getLogger(__name__)
-
 
 class RDSLogDog(object):  # pragma: no cover
 
@@ -16,14 +14,17 @@ class RDSLogDog(object):  # pragma: no cover
         self.s3_dst_prefix_for_logs = config.s3_prefix_for_logs
 
     @staticmethod
-    def setup_logger(level=20):
+    def setup_logger(level='INFO'):
         logger = logging.getLogger()
         handler = logging.StreamHandler()
         formatter = logging.Formatter(
             '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(int(level))
+        # to avoid multiple handlers to be attached to the logger
+        # this happens often with lambda
+        if not logger.handlers or logger.handlers == []:
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+        logger.setLevel(level)
         logger.debug('set loglevel to DEBUG')
         # disable botocore logging in debug level
         logger = logging.getLogger('botocore')
@@ -33,24 +34,24 @@ class RDSLogDog(object):  # pragma: no cover
 
     def sync_logfiles(self, logfiles, logfilehandler):
         for logfile in logfiles:
-            logger.info('copying \'%s\' (%d) ...', logfile.name, logfile.size)
+            logging.info('copying \'%s\' (%d) ...', logfile.name, logfile.size)
             logfilehandler.copy(logfile)
 
     def discover_logfiles_to_copy(self, logfilehandler):
         files_in_s3 = logfilehandler.discover_logfiles_in_s3()
-        logger.debug('found %d files in s3://%s/%s',
-                     len(files_in_s3),
-                     logfilehandler.dst_bucket,
-                     logfilehandler.dst_prefix_instance)
+        logging.debug('found %d files in s3://%s/%s',
+                      len(files_in_s3),
+                      logfilehandler.dst_bucket,
+                      logfilehandler.dst_prefix_instance)
         files_in_rds = logfilehandler.discover_logfiles_in_rds()
-        logger.debug('found %d files in %s',
-                     len(files_in_rds), logfilehandler.rds_instance.name)
+        logging.debug('found %d files in %s',
+                      len(files_in_rds), logfilehandler.rds_instance.name)
         logfiles = logfilehandler.logfiles_to_copy(
             files_in_rds, files_in_s3)
         return logfiles
 
     def process_instance(self, instance):
-        logger.info('processing rds-instance: \'%s\'', instance.name)
+        logging.info('processing rds-instance: \'%s\'', instance.name)
         logfilehandler = LogFileHandler(
             instance,
             self.s3_dst_bucket,
@@ -58,16 +59,16 @@ class RDSLogDog(object):  # pragma: no cover
         setup_s3_destination(logfilehandler.dst_bucket,
                              logfilehandler.dst_prefix_instance)
         logfiles_to_copy = self.discover_logfiles_to_copy(logfilehandler)
-        logger.info('going to copy %d new logfiles ...', len(logfiles_to_copy))
+        logging.info('going to copy %d new logfiles ...', len(logfiles_to_copy))
         self.sync_logfiles(logfiles_to_copy, logfilehandler)
         # write metric / logentry
-        logger.info('synced %d files for \'%s\'',
-                    len(logfiles_to_copy), instance.name)
+        logging.info('synced %d files for \'%s\'',
+                     len(logfiles_to_copy), instance.name)
 
     def run(self):
         discoverer = Discoverer()
         instances = discoverer.discover()
-        logger.info('%d instances discovered.', len(instances))
+        logging.info('%d instances discovered.', len(instances))
         for instance in instances:
             self.process_instance(instance)
         return 0
